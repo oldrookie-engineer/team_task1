c2065ab8fc26627ba1089d75e5dadf1a2ca3310d
oldrookie-engineer <paburofu0023@icloud.com>
2020-05-28 11:58:48 +0900

    権限移動時の確認メールをletter openerに設定

--- app/mailers/change_owner_mailer.rb
+++ app/mailers/change_owner_mailer.rb
@@ -1,8 +1,8 @@
-# class ChangeOwnerMailer < ApplicationMailer
-#   default from: 'from@example.com'
-#   def change_owner_mail(assign)
-#     @assign = assign
-#     mail to: @assign.user.email,
-#     subject: 'チームリーダーの権限移動の確認メール'
-#   end
-# end
+class ChangeOwnerMailer < ApplicationMailer
+  default from: 'from@example.com'
+  def change_owner_mail(assign)
+    @assign = assign
+    mail to: @assign.user.email,
+    subject: 'チームリーダーの権限移動の確認メール'
+  end
+end
